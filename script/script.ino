04d385d9880b5b41a72da9a3e2c8b4a19d2ce2b4
seitvalieva <sseitvalieva@gmail.com>
2024-06-18 22:21:05 +0200

    arduino uno r3 and sensors (c++ arduino)

---
+++ script/script.ino
@@ -0,0 +1,48 @@
+ int button1 = 7; //set the button1 pin
+ int button2 = 8; //set the button2 pin
+ int LED = 4; //set the LED pin
+
+ int buzzer = 3; //set buzzer
+ int motion = 5; // set motion sensor
+
+ bool state1 = 1; //set button1 state
+ bool state2 = 1; //set button2 state
+ 
+ void setup()
+ {
+   pinMode(button1,INPUT);
+   pinMode(button2,INPUT);
+   pinMode(LED,OUTPUT);
+   pinMode(buzzer, OUTPUT);
+   digitalWrite(buzzer,LOW);
+   pinMode(motion, INPUT);
+   Serial.begin(115200);
+  //  Serial.println("bengin the communication");
+}
+
+void loop() {
+  state1 = digitalRead(button1);
+  state2 = digitalRead(button2);
+  digitalWrite(LED,HIGH);
+  if (state1 == 0)
+  {
+    tone(buzzer, 100,50);
+    delay(100);
+    tone(buzzer, 1000,50);
+  }
+  if (state2 == 0)
+  {
+    digitalWrite(LED,HIGH);
+    delay(100);
+    digitalWrite(LED,LOW);
+    delay(100);
+  }
+  if (digitalRead(motion))
+  {
+    Serial.println("test");
+    digitalWrite(LED,HIGH);
+    delay(100);
+    digitalWrite(LED,LOW);
+    delay(100);
+  }
+ }
